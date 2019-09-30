c3f6a3febe0d5dc34d4dd0b5ba1ae13cfc003f03
sparklingspur <54412927+sparklingspur@users.noreply.github.com>
2019-09-30 10:52:05 +0530

    Add files via upload

---
+++ scripting.js
@@ -0,0 +1,24 @@
+/**
+ * Created by krishnan on 27/09/2019.
+ */
+
+ $(document).ready(function(){
+
+
+	  $("#mymenumobOpen").click(function(){
+	    $("#mymenu").css("display","block");
+	    $("#mymenumobClose").css("display","inline-block");
+	    $("#mymenumobOpen").css("display","none");
+	  });
+
+	  $("#mymenumobClose").click(function(){
+	    $("#mymenu").css("display","none");
+	    $("#mymenumobOpen").css("display","inline-block");
+	    $("#mymenumobClose").css("display","none");
+	  });
+
+	  // $( "#faqaccordion" ).accordion({
+   //    	collapsible: true
+   //    });
+
+});

2e43290e78d7613ee9b057615eaea39895a69bc6
sparklingspur <mysparklingspur@gmail.com>
2025-07-29 16:35:17 +0530

    Gmaps Api Key Removed.

--- components/Gmaps.vue
+++ components/Gmaps.vue
@@ -26,8 +26,6 @@ const longitude = 76.65063077499629;
 
 
 const capsUrl = "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d245.04326780414345!2d76.6506308!3d10.6809758!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ba86dcc65d205df%3A0xab44c5e2ccdcf3e!2sCAPS%20Family%20Bakes%20and%20Restaurants!5e0!3m2!1sen!2sin!4v1753707646717!5m2!1sen!2sin";
-
-const mapUrl = `https://www.google.com/maps/embed/v1/place?key=AIzaSyD7Ufia8881UHjohW97UbDPExth1UXwt-I&q=${latitude},${longitude}`;
 </script>
 
 <style scoped>

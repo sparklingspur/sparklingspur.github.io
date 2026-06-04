60727eed262d21a61a43c4d020d813be8b960557
Krishnan K R <krizzzcode@gmail.com>
2026-06-04 12:37:45 +0530

    Second  commit: Scaffold Nuxt 4 project - SreejithTravels

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,4 +4,8 @@ export default defineNuxtConfig({
   modules: ['@nuxtjs/tailwindcss'],
   css: ['~/assets/css/main.css'],
   compatibilityDate: '2024-04-04',
+  app: {
+      baseURL: '/',
+      buildAssetsDir: 'assets',
+    },
 })

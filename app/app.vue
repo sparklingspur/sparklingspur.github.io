527ef3f683d3c601544fc52cae06cd872ca72833
Krishnan K R <krizzzcode@gmail.com>
2026-06-04 11:57:17 +0530

    Initial commit: Scaffold Nuxt 4 project - SreejithTravels

---
+++ app/app.vue
@@ -0,0 +1,16 @@
+<!-- <template>
+  <div>
+    <NuxtRouteAnnouncer />
+    <NuxtWelcome />
+  </div>
+</template> -->
+
+<script setup>
+import '~/assets/css/main.css'
+</script>
+
+<template>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
+</template>
